20254aeec27b8fb6e9b35f6b2de0726093d9abda
James Wong <jameswong@jamess-mbp.home>
2014-08-11 19:28:27 -0400

    Initial Commit

---
+++ NewFoundationProject/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  NewFoundationProject
+//
+//  Created by James Wong on 8/11/14.
+//  Copyright (c) 2014 James Wong. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
